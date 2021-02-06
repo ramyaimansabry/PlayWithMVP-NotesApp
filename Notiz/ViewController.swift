878c8a830106e07d3af3cc3160615fccb5d9239b
Ramy Sabry <ramy.sabri@venturous.solutions>
2021-02-06 14:01:09 +0200

    first commit

---
+++ Notiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notiz
+//
+//  Created by Ramy Sabry on 06/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
