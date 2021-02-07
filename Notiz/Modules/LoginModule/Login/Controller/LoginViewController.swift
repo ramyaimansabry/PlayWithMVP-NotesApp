b145afff05ecad30ad30d97aec73ea6c020f6b8d
Ramy Sabry <ramy.sabri@venturous.solutions>
2021-02-07 23:56:36 +0200

    login & register ui

--- Notiz/Modules/LoginModule/Login/Controller/LoginViewController.swift
+++ Notiz/Modules/LoginModule/Login/Controller/LoginViewController.swift
@@ -9,14 +9,51 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var contentStackView: UIStackView!
+    @IBOutlet weak var signInLabel: UILabel!
+    @IBOutlet weak var textFieldsStackView: UIStackView!
+    @IBOutlet weak var emailTextField: UnderLineTextField!
+    @IBOutlet weak var passwordTextField: UnderLineTextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var dontHaveAccountLabel: UILabel!
+    
     private lazy var presenter = LoginVCPresenter(view: self)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor.red
+        configureViews()
     }
-
+    
+    fileprivate func configureViews() {
+        view.backgroundColor = UIColor.white
+        contentStackView.spacing = min(max(view.frame.height / 12, 35), 45)
+        signInButton.setShadow(withCornerRadius: 10)
+        configureDontHaveAccountLabel()
+    }
+    
+    func configureDontHaveAccountLabel() {
+        let text = "Don't have account? Create now"
+        dontHaveAccountLabel.text = text
+        let underlineAttriString = NSMutableAttributedString(string: text)
+        let range1 = (text as NSString).range(of: "Don't have account?")
+        let range2 = (text as NSString).range(of: "Create now")
+        underlineAttriString.addAttribute(.foregroundColor, value: DesignSystem.current.colors.secondaryColor, range: range1)
+        underlineAttriString.addAttribute(.foregroundColor, value: DesignSystem.current.colors.primaryColor, range: range2)
+        dontHaveAccountLabel.attributedText = underlineAttriString
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(createAccount(_:)))
+        tapGesture.numberOfTouchesRequired = 1
+        dontHaveAccountLabel.isUserInteractionEnabled = true
+        dontHaveAccountLabel.addGestureRecognizer(tapGesture)
+    }
+    
+    // MARK:- IBActions
+    @IBAction func signInButton(_ sender: UIButton) {
+        
+    }
+    
+    @objc func createAccount(_ sender: UITapGestureRecognizer) {
+        Navigator.navigate(form: self, to: .register, with: .push, animated: true)
+    }
 }
 
 extension LoginViewController: LoginVCView {
--- Notiz/Modules/LoginModule/Register/Controller/RegisterViewController.swift
+++ Notiz/Modules/LoginModule/Register/Controller/RegisterViewController.swift
@@ -9,15 +9,43 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var backButtonTopLayoutConstraint: NSLayoutConstraint!
+    @IBOutlet weak var contentStackView: UIStackView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var signUpLabel: UILabel!
+    @IBOutlet weak var textFieldsStackView: UIStackView!
+    @IBOutlet weak var fullNameTextFields: UnderLineTextField!
+    @IBOutlet weak var emailTextField: UnderLineTextField!
+    @IBOutlet weak var passwordTextField: UnderLineTextField!
+    @IBOutlet weak var confirmPasswordTextField: UnderLineTextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     private lazy var presenter = RegisterVCPresenter(view: self)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureViews()
     }
-
-
+    
+    fileprivate func configureViews() {
+        view.backgroundColor = UIColor.white
+        contentStackView.spacing = min(max(view.frame.height / 12, 35), 45)
+        configureSignUpButton()
+    }
+    
+    private func configureSignUpButton() {
+        signUpButton.setShadow(withCornerRadius: 10)
+    }
+    
+    // MARK:- IBActions
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func signUpButtonAction(_ sender: UIButton) {
+        
+    }
+    
 }
 
 extension RegisterViewController: RegisterVCView {
